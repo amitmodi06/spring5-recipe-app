c7eb3b28886623ab1529c8bcc693d72501c18d59
amitmodi06 <amitmodi06@gmail.com>
2020-04-26 22:57:32 +0530

    Added Enum Difficulty and add relationship with Recipe.

--- src/main/java/amit/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/amit/springframework/spring5recipeapp/domain/Recipe.java
@@ -19,8 +19,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //to add
-    /*private Difficulty difficulty;*/
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -93,6 +94,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Byte[] getImage() {
